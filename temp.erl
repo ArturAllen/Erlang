1b167f0602f853231b1433c3e9fe67e3ea53213f
Artur <72237233+ArturAllen@users.noreply.github.com>
2023-01-24 10:46:55 -0300

    Update temp.erl

--- temp.erl
+++ temp.erl
@@ -2,9 +2,17 @@
 -module(temp).
 
 %API
--export([convert/2]).
+-export([convert/1, convert/2]).
 
 convert(F, fahrenheit) ->
   (F - 32) * 5 / 9;
 convert(C, celcius) ->
   C * 9 / 5 + 32.
+
+
+convert({fahrenheit, X}) ->
+  Y = (X - 32) * 5 / 9, %% `,` separates lines of code
+  {celcius, Y};         
+convert({celcius, X}) ->
+  Y = X * 5 / 9 + 32,
+  {fahrenheit, Y}.
