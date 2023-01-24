9e2939541e0789e97bd8e24ffaff2ef1171b1454
Artur <72237233+ArturAllen@users.noreply.github.com>
2023-01-24 11:20:34 -0300

    Create cal.erl

---
+++ cal.erl
@@ -0,0 +1,33 @@
+% hello world program
+-module(cal).
+
+%API
+-export([leap/1]).
+
+leap(Year) ->
+  if
+    Year rem 400 == 0 -> leap;
+    Year rem 100 == 0 -> non_leap;
+    Year rem 4   == 0 -> leap;
+    true -> non_leap
+  end.
+
+days(Month, Year) ->
+  Leap = leap(Year),
+  case Month of
+    jan -> 31;
+    feb when Leap == leap -> 29; %% This is a guard
+    feb -> 28;
+    mar -> 31;
+    apr -> 30;
+    may -> 31;
+    jun -> 30;
+    jul -> 31;
+    aug -> 31;
+    sep -> 30;
+    oct -> 31;
+    nov -> 30;
+    dez -> 31
+  end.
+
+
